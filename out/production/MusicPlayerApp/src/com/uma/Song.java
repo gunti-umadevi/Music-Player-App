18d5c800295b7c7f89f7bf2b0a9ac1493c3e7e76
gunti-umadevi <161290875+gunti-umadevi@users.noreply.github.com>
2024-02-26 15:54:35 +0530

    Create Song.java

---
+++ out/production/MusicPlayerApp/src/com/uma/Song.java
@@ -0,0 +1,27 @@
+package com.hemanth;
+
+public class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
